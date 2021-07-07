378a96b52d327f0c43878fbb5399f08ba3bf0fa7
Radensuaso <andrels9283@gmail.com>
2021-07-07 23:35:09 +0100

    Select a card is in progress

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,18 +1,41 @@
 import { Card } from "react-bootstrap"
 import MyBadge from "./MyBadge"
 
-const SingleBook = (props) => (
-  <Card>
-    <Card.Img className="img-fluid" variant="top" src={props.book.img} />
-    <Card.Body>
-      <Card.Title>{props.book.title}</Card.Title>
-      <Card.Text>
-        <strong>Category: </strong>
-        {props.book.category}
-      </Card.Text>
-      <MyBadge color={"success"} text={"€ " + props.book.price} />
-    </Card.Body>
-  </Card>
-)
+import React, { Component } from "react"
+
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  }
+
+  handleSelect = (e) => {
+    this.setState((prevState) => ({
+      selected: true,
+    }))
+    if (this.state.selected) {
+      e.currentTarget.classList.add("bg-primary", "text-white")
+    }
+  }
+
+  render() {
+    return (
+      <Card onClick={(e) => this.handleSelect(e)}>
+        <Card.Img
+          className="img-fluid"
+          variant="top"
+          src={this.props.book.img}
+        />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>
+            <strong>Category: </strong>
+            {this.props.book.category}
+          </Card.Text>
+          <MyBadge color={"success"} text={"€ " + this.props.book.price} />
+        </Card.Body>
+      </Card>
+    )
+  }
+}
 
 export default SingleBook
